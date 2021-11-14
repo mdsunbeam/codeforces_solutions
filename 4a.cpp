b26da6b25af3d22c1709e1cf9f72fa21e201ec2d
mdsunbeam <mdsunbeam3.14@gmail.com>
2021-11-14 02:57:10 -0600

    formatting

--- 4a.cpp
+++ 4a.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-int main() {
+
+int main()
+{
     int w;
     cin >> w;
-    if (w % 2 == 0 && w != 2) {
+    if (w % 2 == 0 && w != 2)
+    {
         cout << "YES";
-    } else {
+    }
+    else
+    {
         cout << "NO";
     }
 }
